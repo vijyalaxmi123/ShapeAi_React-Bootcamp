6f154e6b800691031b66b0097fcee0e2466cf383
vijyalaxmi123 <72075931+vijyalaxmi123@users.noreply.github.com>
2021-06-05 13:34:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Note_2 from "./Note_2";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note_2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note_2.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note_2() {
+  return (
+    <div className="note">
+      <h1>Machine Learning Bootcamp!! </h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including Python,basic machine learning.
+      </p>
+
+      
+    </div>
+  );
+}
+
+export default Note_2;
